8c4145837815afda7db81e00df7dfeda270c6876
litetex <40789489+litetex@users.noreply.github.com>
2024-10-31 17:47:30 +0100

    Update build.sh

--- fdroid-repo-generator/build.sh
+++ fdroid-repo-generator/build.sh
@@ -1,15 +1,27 @@
-echo "(Re)Building repo"
+echo "Cleaning"
 rm -rf /repo/*
 
 mkdir -p /repo/repo
 
+echo "Copying apks"
 cp -a /apks/*.apk /repo/repo/
+echo "Copying repobase"
 cp -a /repobase/. /repo/
 
+echo "SEDing config.yml"
+sed -i -e 's/$REPO_KEYALIAS/'\""$REPO_KEYALIAS"\"'/' /repo/config.yml
+sed -i -e 's/$KEYSTOREPASS/'\""$KEYSTOREPASS"\"'/' /repo/config.yml
+sed -i -e 's/$KEYPASS/'\""$KEYPASS"\"'/' /repo/config.yml
+sed -i -e 's/$KEYDNAME/'\""$KEYDNAME"\"'/' /repo/config.yml
+
+echo "Ensuring that permissions are correct"
 chown root:root /repo/config.yml
 chmod 600 /repo/config.yml
 chown root:root /repo/keystore.p12
 chmod 600 /repo/keystore.p12
 
+echo "Updating..."
 fdroid update -v --use-date-from-apk 
+
+echo "Deploying..."
 fdroid deploy
