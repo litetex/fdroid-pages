c05ad5203631bb16e56cb57fc383bde9a9e0fd37
litetex <40789489+litetex@users.noreply.github.com>
2024-10-31 17:38:24 +0100

    Update build.sh

--- fdroid-repo-generator/build.sh
+++ fdroid-repo-generator/build.sh
@@ -8,6 +8,8 @@ cp -a /repobase/. /repo/
 
 chown root:root /repo/config.yml
 chmod 600 /repo/config.yml
+chown root:root /repo/keystore.p12
+chmod 600 /repo/keystore.p12
 
 fdroid update -v --use-date-from-apk 
 fdroid deploy
