bd94b2c89a1b23eeda70cd55435a7cc354dc4941
litetex <40789489+litetex@users.noreply.github.com>
2024-10-31 17:31:50 +0100

    Fix permissions

--- fdroid-repo-generator/build.sh
+++ fdroid-repo-generator/build.sh
@@ -6,5 +6,8 @@ mkdir -p /repo/repo
 cp -a /apks/*.apk /repo/repo/
 cp -a /repobase/. /repo/
 
+chown root:root /repo/config.yml
+chmod 600 /repo/config.yml
+
 fdroid update --use-date-from-apk 
 fdroid deploy
